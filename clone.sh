77da3387cef76c4a424800988eac44968edc26b9
Aniket Sharma <89375908+officialtechrom@users.noreply.github.com>
2022-03-08 12:49:13 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
   #remove_HALS
-     #rm -rf hardware/qcom-caf/wlan
-     #rm -rf hardware/qcom-caf/msm8996/audio
-     #rm -rf hardware/qcom-caf/msm8996/display
-     #rm -rf hardware/qcom-caf/msm8996/media
+     rm -rf hardware/qcom-caf/wlan
+     rm -rf hardware/qcom-caf/msm8996/audio
+     rm -rf hardware/qcom-caf/msm8996/display
+     rm -rf hardware/qcom-caf/msm8996/media
    
    #remove_old_trees
-     #rm -rf device/xiaomi/onclite
-     #rm -rf vendor/xiaomi/onclite
-     #rm -rf kernel/xiaomi/onclite 
+     rm -rf device/xiaomi/onclite
+     rm -rf vendor/xiaomi/onclite
+     rm -rf kernel/xiaomi/onclite 
    
    #clone_HALS
-     #git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_audio.git -b 12 hardware/qcom-caf/msm8996/audio
-     #git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_media.git -b 12 hardware/qcom-caf/msm8996/media
-     #git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_display.git -b 12 hardware/qcom-caf/msm8996/display
-     #git clone https://github.com/afterallafk/hardware_qcom-caf_wlan.git -b 12 hardware/qcom-caf/wlan
+     git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_audio.git -b 12 hardware/qcom-caf/msm8996/audio
+     git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_media.git -b 12 hardware/qcom-caf/msm8996/media
+     git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_display.git -b 12 hardware/qcom-caf/msm8996/display
+     git clone https://github.com/afterallafk/hardware_qcom-caf_wlan.git -b 12 hardware/qcom-caf/wlan
     
     #clone_trees
-     #git clone https://github.com/officialtechrom/device_xiaomi_onclite_komodo.git -b 12 device/xiaomi/onclite
-     #git clone https://github.com/officialtechrom/vendor_xiaomi_onclite.git -b 12 vendor/xiaomi/onclite
-     #git clone https://github.com/officialtechrom/android_kernel_xiaomi_onclite.git -b lineage-18.1 kernel/xiaomi/onclite
+     git clone https://github.com/officialtechrom/device_xiaomi_onclite_raven12.git -b 12 device/xiaomi/onclite
+     git clone https://github.com/officialtechrom/vendor_xiaomi_onclite.git -b 12 vendor/xiaomi/onclite
+     git clone https://github.com/officialtechrom/android_kernel_xiaomi_onclite.git -b lineage-18.1 kernel/xiaomi/onclite
     
     #Launcher
-    rm -rf packages/apps/Launcher3
+    #rm -rf packages/apps/Launcher3
     
     #clone_Launcher
-    git clone https://github.com/VoltageOS/packages_apps_Launcher3.git packages/apps/Launcher3
+    #git clone https://github.com/VoltageOS/packages_apps_Launcher3.git packages/apps/Launcher3
